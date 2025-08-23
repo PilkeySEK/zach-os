7e2d54aa1ec1bad722e1be3bc8963dd655073a5e
PilkeySEK <PilkeySEK@gmx.de>
2025-08-23 16:56:56 +0200

    rename myos to zachos

--- iso.sh
+++ iso.sh
@@ -6,10 +6,10 @@ mkdir -p isodir
 mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
 
-cp sysroot/boot/myos.kernel isodir/boot/myos.kernel
+cp sysroot/boot/zachos.kernel isodir/boot/zachos.kernel
 cat > isodir/boot/grub/grub.cfg << EOF
-menuentry "myos" {
-	multiboot /boot/myos.kernel
+menuentry "zachos" {
+	multiboot /boot/zachos.kernel
 }
 EOF
-grub-mkrescue -o myos.iso isodir
+grub-mkrescue -o zachos.iso isodir
